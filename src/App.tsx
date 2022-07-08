20b84f611bd953e8d96c2407575b95c4b8a5860a
Thomas O'Neill <tom@opensystemslab.io>
2022-07-08 13:22:14 +0100

    try to integrate with mapbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { createRoot, extend } from "@react-three/fiber"
-import mapboxgl from "mapbox-gl"
+import mapboxgl, { AnyLayer } from "mapbox-gl"
 import "mapbox-gl/dist/mapbox-gl.css"
 import { useEffect, useState } from "react"
 import * as THREE from "three"
+import ThreeApp from "./ThreeApp"
 
 extend(THREE)
 
@@ -24,9 +25,10 @@ const App = () => {
       style: "mapbox://styles/mapbox/streets-v11", // style URL
       center: [-0.0804, 51.5145], // starting position [lng, lat]
       zoom: 18, // starting zoom
-      projection: {
-        name: "globe",
-      },
+      antialias: true,
+      // projection: {
+      //   name: "globe",
+      // },
     })
 
     // const size = { width: window.innerWidth, height: window.innerHeight }
@@ -36,18 +38,39 @@ const App = () => {
 
     const root = createRoot(canvas)
 
+    const customLayer: AnyLayer = {
+      id: "custom_layer",
+      type: "custom",
+      renderingMode: "3d",
+      onAdd: function (map, context) {
+        root.configure({
+          gl: {
+            alpha: true,
+            antialias: true,
+            autoClear: false,
+            canvas: map.getCanvas(),
+            context,
+            outputEncoding: THREE.sRGBEncoding,
+            preserveDrawingBuffer: false,
+          },
+          size: {
+            width: map.getCanvas().clientWidth,
+            height: map.getCanvas().clientHeight,
+          },
+        })
+
+        map.repaint = false
+      },
+
+      render: function (gl, matrix) {
+        root.render(<ThreeApp />)
+        map.triggerRepaint()
+      },
+    }
+
     map.on("style.load", () => {
       map.setFog({}) // Set the default atmosphere style
-      map.addLayer({
-        id: "custom_layer",
-        type: "custom",
-        renderingMode: "3d",
-        onAdd: function (map, mbxContext) {
-          root.configure({ gl: mbxContext })
-        },
-
-        render: function (gl, matrix) {},
-      })
+      map.addLayer(customLayer)
     })
 
     return () => root.unmount()
