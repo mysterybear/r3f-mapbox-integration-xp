eb6b717d0e771ef76c8938244fd2c05c909471ff
Thomas O'Neill <tom@opensystemslab.io>
2022-07-08 09:19:44 +0100

    not rendering to custom canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createRoot } from "@react-three/fiber"
+import mapboxgl from "mapbox-gl"
+import "mapbox-gl/dist/mapbox-gl.css"
+import { useEffect, useRef } from "react"
+import ThreeApp from "./ThreeApp"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN ?? ""
+
+const fullWidthHeight = {
+  width: "100%",
+  height: "100%",
 }
 
-export default App;
+const App = () => {
+  const mapRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (!mapRef.current) return
+
+    const map = new mapboxgl.Map({
+      container: mapRef.current, // container ID
+      style: "mapbox://styles/mapbox/streets-v11", // style URL
+      center: [-0.0804, 51.5145], // starting position [lng, lat]
+      zoom: 18, // starting zoom
+      projection: {
+        name: "globe",
+      },
+    })
+
+    map.on("style.load", () => {
+      map.setFog({}) // Set the default atmosphere style
+    })
+
+    // const size = { width: window.innerWidth, height: window.innerHeight }
+    const canvas = mapRef.current.querySelector("canvas")
+
+    if (!canvas) return
+
+    const root = createRoot(canvas)
+
+    root.render(<ThreeApp />)
+  }, [])
+
+  return (
+    <div style={{ position: "absolute", ...fullWidthHeight }}>
+      <div ref={mapRef} style={fullWidthHeight} />
+    </div>
+  )
+}
+
+export default App
---
+++ src/ThreeApp.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const ThreeApp = () => {
+  return (
+    <mesh>
+      <boxBufferGeometry />
+      <meshBasicMaterial color="tomato" />
+    </mesh>
+  )
+}
+
+export default ThreeApp
